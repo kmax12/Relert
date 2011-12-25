500dcb0c90f013d94a70d0dea0c3790fd30b9f28
Max Kanter <kmax12@gmail.com>
2011-12-25 00:00:31 -0500

    added add url api

--- server.js
+++ server.js
@@ -34,16 +34,16 @@ app.get('/', function (req, response) {
 });
 
 app.get('/add/:url', function (req, response) {
-	redis.GET('count', function(err, res){
-        var num = res,
-		base64 = decToBase64(num);
+	//redis.GET('count', function(err, res){
+        //var num = res,
+		//base64 = decToBase64(num);
 		//console.log('num: ' + num);
 		//console.log('base: ' + base64);
-		redis.INCR('count', function(){
-			redis.SET(base64, req.params.url);
-			response.write('{url:'+base64+'}');
+		//redis.INCR('count', function(){
+			//redis.SET(base64, req.params.url);
+			response.write('{url:'+req.params.url+'}');
 			response.end();
-		});		
+		//});		
     });
 });
 
