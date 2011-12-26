df3fd28f938508a315a4ee8c70c1474c44856f8e
Max Kanter <kmax12@gmail.com>
2011-12-26 18:36:49 -0500

    blah

--- webroot/frame.js
+++ webroot/frame.js
@@ -1,9 +1,12 @@
+var messageSent = false;
 $(window).ready(function(){
 	resize();
 	$(window).resize(resize);
 	
 	$('#close').click(function(){
-		$.post("http://relert.herokuapp.com/done/"+window.hex, {message: false} );	
+		if(!messageSent){
+			$.post("http://relert.herokuapp.com/done/"+window.hex, {message: false} );	
+		}
 	});
 	
 	$('#send').click(function(){
@@ -13,6 +16,7 @@ $(window).ready(function(){
 				alert('Please enter message');
 				$message.focus();
 		} else {
+			messageSent = true;
 			$.post("http://relert.herokuapp.com/done/"+window.hex, {message: true, messageBody: val}, function(res){
 				res = JSON.parse(res);
 				
